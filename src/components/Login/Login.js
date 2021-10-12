ad84300c3f5ad04b6e71f95eb07d5056f8536a5f
tanjil <tanjil14@cse.pstu.ac.bd>
2021-10-12 10:56:21 +0600

    Folder structure

---
+++ src/components/Login/Login.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from "react-router-dom";
+const Login = () => {
+    return (
+        <div>
+            <h2>Please Login!</h2>
+            <button>Google Sign in</button>
+            <br />
+            <Link to="/register">New User?</Link>
+        </div>
+    );
+};
+
+export default Login;
